e78b8ed36980152faacd06e2b5535fc4c0cf4f9f
Irsyad Jamal Pratama Putra <irsyadjpratamap@gmail.com>
2017-10-15 18:20:19 +0700

    Add email and phone number field. Modify column name. Generate get set

--- src/main/java/id/barangka/soeru/dao/User.java
+++ src/main/java/id/barangka/soeru/dao/User.java
@@ -1,5 +1,9 @@
 package id.barangka.soeru.dao;
 
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import javax.persistence.*;
 import java.sql.Timestamp;
 
@@ -10,27 +14,125 @@ public class User {
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
-    @Column
+    @Column(name = "username")
     private String username;
 
-    @Column
+    @Column(name = "password")
     private String password;
 
-    @Column
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "status")
     private Character status;
 
-    @Column
+    @Column(name = "privilege")
     private Character privilege;
 
-    @Column
+    @Column(name = "created_date")
+    @DateTimeFormat
+    @CreatedDate
     private Timestamp createdDate;
 
-    @Column
+    @Column(name = "updated_date")
+    @DateTimeFormat
+    @LastModifiedDate
     private Timestamp updateadDate;
 
-    @Column
+    @Column(name = "created_by")
     private String createdBy;
 
-    @Column
+    @Column(name = "updated_by")
     private String updatedBy;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Character getStatus() {
+        return status;
+    }
+
+    public void setStatus(Character status) {
+        this.status = status;
+    }
+
+    public Character getPrivilege() {
+        return privilege;
+    }
+
+    public void setPrivilege(Character privilege) {
+        this.privilege = privilege;
+    }
+
+    public Timestamp getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Timestamp createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Timestamp getUpdateadDate() {
+        return updateadDate;
+    }
+
+    public void setUpdateadDate(Timestamp updateadDate) {
+        this.updateadDate = updateadDate;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public String getUpdatedBy() {
+        return updatedBy;
+    }
+
+    public void setUpdatedBy(String updatedBy) {
+        this.updatedBy = updatedBy;
+    }
 }
