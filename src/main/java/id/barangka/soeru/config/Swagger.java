b67defffc8455e9959c0fdd843f62bb673f55b44
Irsyad Jamal Pratama Putra <irsyadjpratamap@gmail.com>
2017-10-15 18:28:21 +0700

    Configuration for Swagger

--- src/main/java/id/barangka/soeru/config/Swagger.java
+++ src/main/java/id/barangka/soeru/config/Swagger.java
@@ -1,9 +1,36 @@
 package id.barangka.soeru.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
 public class Swagger {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("id.barangka.soeru"))
+                .paths(PathSelectors.ant("/controller/*"))
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        ApiInfo apiInfo = new ApiInfo(
+                "Soeru API",
+                "Payment Gateway for PKL in Indonesia.",
+                "API v1.0.0",
+                "Terms of Service",
+                "irsyadjpratamap@gmail.com",
+                "License of API",
+                "API license URL");
+        return apiInfo;
+    }
 }
